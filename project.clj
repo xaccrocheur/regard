fe1ba69c94ee2f9340a60b24f395da7ddd412ba8
xaccrocheur <xaccrocheur@gmail.com>
2013-08-24 19:01:21 +0100

    1st real commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject regard "1.0.0-SNAPSHOT"
+  :description "Spits out regexps, given keywords"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                [frak "0.1.2"]]
+  :main regard.core
+)
---
+++ test/regard/test/core.clj
@@ -0,0 +1,6 @@
+(ns regard.test.core
+  (:use [regard.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
