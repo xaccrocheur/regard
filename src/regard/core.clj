6a0f11e4c063aae9f229b1ce792157b798187ceb
xaccrocheur <xaccrocheur@gmail.com>
2013-08-25 11:07:52 +0100

    trampoline run

--- src/regard/core.clj
+++ src/regard/core.clj
@@ -1,6 +1,26 @@
 (ns regard.core)
 
+(require 'frak)
+
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (frak/pattern ["foo" "bar" "baz" "quux"])
+  (println "Hello, World! arg? %s" args))
+
+(def command (atom ""))
+
+(defn print-prompt []
+  (print "prompt> ")
+  (flush)
+)
+
+(defn ask-for-input []
+    (print-prompt)
+    (let [x (str (read-line))]
+      (println (str "User input: " x))
+      (reset! command x)
+    )
+)
+
+(ask-for-input)
