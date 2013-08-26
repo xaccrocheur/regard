34407a434d04872c800055caed67aacac0061776
xaccrocheur <xaccrocheur@gmail.com>
2013-08-26 12:45:10 +0100

    uberjar-name

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                 [frak "0.1.3"]]
   :main regard.core
-  :aot [regard.core])
-
-;; plop
+  :aot [regard.core]
+  :uberjar-name "regard.jar")
