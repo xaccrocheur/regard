277550d2699eb9f1f893e9ac2423f6404cc60cd0
xaccrocheur <xaccrocheur@gmail.com>
2013-08-26 12:41:01 +0100

    clojure 1.5.1 & frak 0.1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject regard "1.0.0-SNAPSHOT"
   :description "Spits out regexps, given keywords"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                [frak "0.1.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                [frak "0.1.3"]]
   :main regard.core
   :aot [regard.core])
 
